7fe79f7224056ba7300f8c58a09f4e3f2d9b583c
Matheus Martins <matheusms280100@gmail.com>
2020-04-20 02:49:41 -0300

    ImageModel added

--- lib/src/models/image_model.dart
+++ lib/src/models/image_model.dart
@@ -4,7 +4,6 @@ class ImageModel {
   String title;
 
   ImageModel(this.id, this.url, this.title);
-
   ImageModel.fromJson(Map<String, dynamic> parsedJson){
     id = parsedJson['id'];
     url = parsedJson['url'];
