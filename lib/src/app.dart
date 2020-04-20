660662e0f19f7e2bb5850e3f60c1ec6604f372ec
Matheus Martins <matheusms280100@gmail.com>
2020-04-20 04:44:27 -0300

    Simple app. It show images for us

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -24,6 +24,7 @@ class _AppState extends State<App> {
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
 import '../models/image_model.dart';
 
-class ImageList extends StatelessWidget{
-  
+class ImageList extends StatelessWidget {
   final List<ImageModel> images;
 
   ImageList(this.images);
-  
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: images.length,
-      itemBuilder: (context, int index){
+      itemBuilder: (context, int index) {
         return Container(
           margin: EdgeInsets.only(left: 20, right: 20, top: 20),
-          child: Image.network(images[index].url),
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 5),
+                child: Image.network(images[index].url),
+              ),
+              Text(images[index].title),
+            ],
+          ),
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(5)),
         );
       },
     );
