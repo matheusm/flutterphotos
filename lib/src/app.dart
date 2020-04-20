808216490f04ab0477a5d95b99a7924efad468a3
Matheus Martins <matheusms280100@gmail.com>
2020-04-20 03:30:39 -0300

    Starting to use http and a little bit of logic

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    var response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () {
-            print('hi');
-          },
+          onPressed: fetchImage,
         ),
         appBar: AppBar(
           title: Text('Lets see some images!'),
